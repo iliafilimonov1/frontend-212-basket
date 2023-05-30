28733e28ea8bfbf22ba7645126ee488fd98f6a64
Ilia Filimonov <9991571155i@gmail.com>
2023-05-30 19:40:02 +0400

    rewrite load function to async. edited build

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-import { nanoid } from 'nanoid';
-
 /* модалка basket */
 const modal = document.querySelector('#modal-basket'); // получение доступа к модалке
 const openModal = document.querySelector('#open-basket'); // доступ к кнопке
@@ -42,55 +40,61 @@ function init() {
 }
 
 
-// функция подгрузки данных
-function loadJSON() {
-  fetch('http://localhost:3000/products')
-    .then(response => response.json())
-    .then(data => {
-      let html = ``;
-      data.forEach(product => {
-        html += `
-          <div class="main-card">
-            <div class="card-image">
-              <img src="${product?.imgSrc}" alt="image">
-      
-              <div class="card-wishlist">
-                <div class="wishlist-rating">
-        
-                  <div class="rating-img">
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M16 6.12414H9.89333L8 0L6.10667 6.12414H0L4.93333 9.90345L3.06667 16L8 12.2207L12.9333 16L11.04 9.87586L16 6.12414Z"
-                        fill="#FFCE31" />
-                    </svg>
-                  </div>
-        
-                  <span class="rating-amount">${product?.rating}</span>
-                </div>
-        
-                <svg class="whishlist-heart" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z">
-                  </path>
-                </svg>
-              </div>
-            </div>
-      
-            <h3 class="card-name">${product?.name}</h3>
-      
-            <p class="card-category">${product?.category}</p>
 
-            <p class="card-price">${product?.price}</p>
-      
-            <button class="btn btn-primary">Add to cart</button>
-          </div>
-        `
-      })
-      productList.innerHTML = html;
-    })
-    .catch(error => {
-      console.log(error);
-    })
+async function loadJSON() {
+  try {
+    const response = await fetch('http://localhost:3000/products');
+    const data = await response.json();
+
+    console.log(data);
+
+    let html = '';
+
+    if (data && Array.isArray(data)) {
+      data.forEach(product => {
+
+        html += `
+            <div class="main-card">
+              <div class="card-image">
+                <img src="${product?.imgSrc}" alt="image">
+  
+                <div class="card-wishlist">
+                  <div class="wishlist-rating">
+  
+                    <div class="rating-img">
+                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M16 6.12414H9.89333L8 0L6.10667 6.12414H0L4.93333 9.90345L3.06667 16L8 12.2207L12.9333 16L11.04 9.87586L16 6.12414Z"
+                          fill="#FFCE31" />
+                      </svg>
+                    </div>
+  
+                    <span class="rating-amount">${product?.rating}</span>
+                  </div>
+  
+                  <svg class="whishlist-heart" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z">
+                    </path>
+                  </svg>
+                </div>
+              </div>
+  
+              <h3 class="card-name">${product?.name}</h3>
+  
+              <p class="card-category">${product?.category}</p>
+  
+              <p class="card-price">${product?.price}</p>
+  
+              <button class="btn btn-primary">Add to cart</button>
+            </div>
+          `
+      });
+    }
+    productList.insertAdjacentHTML('beforeend', html);
+  } catch (error) {
+    console.error('Ошибка загрузки данных:', error);
+  }
 }
 
 
@@ -101,7 +105,7 @@ function purchaseProduct(e) {
 
     geProductInfo(product); // извлечение данных отдельной карточки
 
-    loadCart(); // загрузка товара
+    //loadCart(); // загрузка товара
   }
 }
 
@@ -180,7 +184,7 @@ function addToBasketList(product) {
 // удаление товара из DOM корзины
 function deleteProduct(e) {
   let basketItem;
-  
+
   if (e.target.tagName === "BUTTON") {
     basketItem = e.target.parentElement.parentElement.parentElement; // получение доступа к родителю всей карточки
     basketItem.remove();
